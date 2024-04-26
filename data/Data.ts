0481577c3b3c5d04303a4aafd2f4f9764f2eded9
alvinfdl21 <alvinfdl21@gmail.com>
2024-04-26 18:00:53 +0700

    add: user static data

---
+++ data/Data.ts
@@ -0,0 +1,12 @@
+import User from "@/types/User";
+
+export const user: User = {
+  name: "Alvin Fadli Dwi Mulya",
+  title: "Software Engineer",
+  email: "alvin.fadli@outlook.com",
+  description:
+    "Final-year software engineering technology student with one year of professional experience. Skilled in frontend development, specifically with React and Svelte. Looking to apply my skills and knowledge in a collaborative environment.",
+  github_link: "https://github.com/alvinfadli",
+  linked_link: "https://www.linkedin.com/in/alvinfadli/.",
+  cv_link: "https://www.google.com",
+};
