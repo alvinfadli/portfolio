c81441c3193272f1ba12ce47efe5a9c570004cf2
alvinfdl21 <alvinfdl21@gmail.com>
2024-04-26 18:00:43 +0700

    add: user types

---
+++ types/User.ts
@@ -0,0 +1,11 @@
+interface User {
+  name: string;
+  title: string;
+  email: string;
+  description: string;
+  github_link: string;
+  linked_link: string;
+  cv_link: string;
+}
+
+export default User;
