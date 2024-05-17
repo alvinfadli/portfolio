baf6192a28072d55d2c981c091019f0a11bb7f63
alvinfdl21 <alvinfdl21@gmail.com>
2024-05-18 01:58:13 +0700

    fix: mobile responsiveness

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,14 +7,18 @@ const Hero = () => {
   const router = useRouter();
 
   return (
-    <div className="relative py-20 md:py-0 md:h-screen flex flex-col items-center justify-center px-5">
+    <div className="relative py-20 md:py-0 h-screen flex flex-col items-center justify-center px-5">
       <div className={`flex w-full`}>
         <div className="w-full  flex flex-col ">
-          <h1 className=" flex justify-end text-2xl md:text-3xl xl:text-5xl font-medium">
-            <div className="w-11/12">
-              <p>Turn ideas into innovative products.</p>
-              <p className="text-right">expertise in digital craftsmanship:</p>
-              <p className="text-right">
+          <h1 className="flex justify-end text-2xl md:text-3xl xl:text-5xl font-medium">
+            <div className="sm:w-11/12">
+              <p className="text-center sm:text-left">
+                Turn ideas into innovative products.
+              </p>
+              <p className="text-center sm:text-right">
+                expertise in digital craftsmanship:
+              </p>
+              <p className="text-center sm:text-right">
                 development, design, research, and beyond.
               </p>
             </div>
