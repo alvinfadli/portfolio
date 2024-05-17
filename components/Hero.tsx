b520e05bdf09ad42e35eee407a6337e6b9927e8a
alvinfdl21 <alvinfdl21@gmail.com>
2024-05-18 01:53:11 +0700

    add: hero content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,18 +9,16 @@ const Hero = () => {
   return (
     <div className="relative py-20 md:py-0 md:h-screen flex flex-col items-center justify-center px-5">
       <div className={`flex w-full`}>
-        <div className="w-full  flex flex-col justify-center ">
-          <h1 className="text-2xl md:text-3xl xl:text-7xl font-serif font-bold mb-2 ">
-            {user?.name}
+        <div className="w-full  flex flex-col ">
+          <h1 className=" flex justify-end text-2xl md:text-3xl xl:text-5xl font-medium">
+            <div className="w-11/12">
+              <p>Turn ideas into innovative products.</p>
+              <p className="text-right">expertise in digital craftsmanship:</p>
+              <p className="text-right">
+                development, design, research, and beyond.
+              </p>
+            </div>
           </h1>
-          <p className="mb-5 font-semibold">{user?.title}</p>
-          <p className="mb-2">{user?.description}</p>
-          <div className="mt-5">
-            <Button
-              text="Download CV"
-              onClick={() => router.push(user?.cv_link)}
-            />
-          </div>
         </div>
       </div>
     </div>
