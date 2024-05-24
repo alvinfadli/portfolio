de2fef409fc607e4054539939c2a9ae8300122a4
alvinfdl21 <alvinfdl21@gmail.com>
2024-05-24 18:37:28 +0700

    feat: add maintenance msg to project page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,9 +1,20 @@
 "use client";
-
+import Image from "next/image";
 export default function Project() {
   return (
     <>
-      <div className="min-h-screen py-20 px-3"></div>
+      <div className="min-h-screen py-20 px-3 flex justify-center items-center">
+        <div className="">
+          <Image
+            src="/on-progress-img.png"
+            alt="image description"
+            width={250}
+            height={250}
+            sizes="100vw"
+          />
+          <p className="text-center">Working on it.</p>
+        </div>
+      </div>
     </>
   );
 }
