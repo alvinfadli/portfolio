acbcbc637488df1130dcc3249a00d1e16b1fac80
alvinfdl21 <alvinfdl21@gmail.com>
2024-05-24 07:05:06 +0700

    add: resume link

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import Button from "./ui/Button";
 import { FileDown, UserRound } from "lucide-react";
+import Link from "next/link";
 
 const Hero = () => {
   const router = useRouter();
@@ -19,12 +20,22 @@ const Hero = () => {
               <p className="text-center sm:text-right">
                 development, design, research, and beyond.
               </p>
+              <div className="flex flex-col md:flex-row w-1/2 md:w-full mx-auto justify-center md:justify-end my-5">
+                <Link
+                  href={
+                    "https://docs.google.com/document/d/1eIJ8GaeF1q7-3LQrcEF6PhJdVpvcobrIBpof8kREe18/edit?usp=sharing"
+                  }
+                  target="_blank"
+                  className="text-sm flex justify-center items-center gap-0.5 py-2 px-5 bg-neutral-900  text-white border shadow-lg rounded-[5px]"
+                >
+                  <span>
+                    <FileDown size={20} />
+                  </span>
+                  My Resume
+                </Link>
+              </div>
             </div>
           </h1>
-          {/* <div className="flex flex-col md:flex-row w-1/2 md:w-full mx-auto justify-center md:justify-end my-5 gap-2">
-            <Button text="My Resume" icon={<FileDown size={20} />} />
-            <Button text="About Me" icon={<UserRound size={20} />} />
-          </div> */}
         </div>
       </div>
     </div>
